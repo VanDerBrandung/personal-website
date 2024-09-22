a0e9d3389c537989467e04937fc351b4abd5daee
luisschmitt01 <luisschmitt01@gmail.com>
2024-09-22 21:43:52 +0200

    add new pictures and update texts

--- src/app/[lng]/about/page.tsx
+++ src/app/[lng]/about/page.tsx
@@ -61,7 +61,7 @@ export default async function About({
                 src={portraitImage}
                 alt=''
                 sizes='(min-width: 1024px) 32rem, 20rem'
-                className='aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800'
+                className='aspect-square rounded-3xl bg-zinc-100 object-cover dark:bg-zinc-800'
               />
             </FadeIn>
           </div>
